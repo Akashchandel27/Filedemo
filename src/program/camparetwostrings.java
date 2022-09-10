978559bae4403eedf085f8d5eaeb7e006e070af4
Akashchandel27 <akashchandel27@gmail.com>
2022-09-10 23:18:06 +0530

    new string added

--- src/program/camparetwostrings.java
+++ src/program/camparetwostrings.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class camparetwostrings {
 
 	public static void main(String[] args) {
-		String s1, s2;
+		// take two strings s1 and s2
+		String s1, s2,s3;
 
+		// scan here
 		Scanner in = new Scanner(System.in);
 
 		System.out.println("Enter string s1");
 		s1 = in.nextLine();
 		System.out.println("Enter string s2");
 		s2 = in.nextLine();
-
+		System.out.println("Enter string s3");
+		s3= in.nextLine();
+		
+		
 		if (s1.compareTo(s2) > 0) {
 			System.out.println("S1 is greater");
 		} else if (s1.compareTo(s2) < 0) {
